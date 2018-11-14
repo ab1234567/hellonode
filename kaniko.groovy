2441cc6d69203aae3163b1c6855444c62303f0d6
ab1234567 <mailtooab@gmail.com>
2018-11-14 18:57:37 +0530

    Add files via upload

---
+++ kaniko.groovy
@@ -0,0 +1,42 @@
+def label = "kaniko-${UUID.randomUUID().toString()}"
+
+podTemplate(name: 'kaniko', label: label, yaml: """
+kind: Pod
+metadata:
+  name: kaniko
+spec:
+  containers:
+  - name: kaniko
+    image: gcr.io/kaniko-project/executor:debug
+    imagePullPolicy: Always
+    command:
+    - /busybox/cat
+    tty: true
+    volumeMounts:
+      - name: jenkins-docker-cfg
+        mountPath: /root
+  volumes:
+  - name: jenkins-docker-cfg
+    projected:
+      sources:
+      - secret:
+          name: regcred
+          items:
+            - key: .dockerconfigjson
+              path: .docker/config.json
+"""
+  ) {
+
+  node(label) {
+    stage('Build with Kaniko') {
+      git 'https://github.com/ab1234567/hellonode.git'
+      container(name: 'kaniko', shell: '/busybox/sh') {
+        withEnv(['PATH+EXTRA=/busybox']) {
+          sh '''#!/busybox/sh
+          /kaniko/executor -f `pwd`/Dockerfile -c `pwd` --insecure-skip-tls-verify --cache=true --destination=mydockerregistry:5000/myorg/myimage
+          '''
+        }
+      }
+    }
+  }
+}
