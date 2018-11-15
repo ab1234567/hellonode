4a487590a1cf9922ef1b01057928f9ac20b6bb33
ab1234567 <mailtooab@gmail.com>
2018-11-15 16:07:20 +0530

    Update kaniko.groovy

--- kaniko.groovy
+++ kaniko.groovy
@@ -1,3 +1,13 @@
+import com.cloudbees.plugins.flow.*;
+import org.jenkinsci.plugins.gitclient.Git;
+import org.jenkinsci.plugins.gitclient.GitClient;
+import  hudson.plugins.promoted_builds.*;
+import org.jvnet.hudson.plugins.groovypostbuild.*;
+import org.jenkinsci.plugins.scriptsecurity.scripts.*;
+import org.jenkinsci.plugins.scriptsecurity.sandbox.groovy.*;
+import hudson.model.RootAction;
+import org.jvnet.hudson.plugins.groovypostbuild.*;
+
 def label = "kaniko-${UUID.randomUUID().toString()}"
 
 podTemplate(name: 'kaniko', label: label, yaml: """
