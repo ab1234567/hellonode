97745732facd970d78b50bdf594ebaa0ceb942d8
ab1234567 <mailtooab@gmail.com>
2018-11-15 17:19:33 +0530

    Update kaniko.groovy

--- kaniko.groovy
+++ kaniko.groovy
@@ -1,20 +1,34 @@
-def label = "mypod-${UUID.randomUUID().toString()}"
-podTemplate(label: label, containers: [
-    containerTemplate(name: 'Kaniko', image: 'gcr.io/kaniko-project/executor:debug', ttyEnabled: true, command: '/busybox/cat'),
-    containerTemplate(name: 'Build', image: 'golang:1.8.0', ttyEnabled: true, command: 'cat')
-  ]) {
+package com.foo.utils
+
+public void Kaniko(body) {
+  podTemplate(label: label,
+        containers: [containerTemplate(name: 'Kaniko', image: 'gcr.io/kaniko-project/executor:debug', ttyEnabled: true, command: '/busybox/cat')],
+        volumes: [hostPathVolume(hostPath: '/var/run/docker.sock', mountPath: '/var/run/docker.sock')]) {
+    body(
+         {
 
     node(label) {
-        stage('Get a Maven project') {
+        stage('Kaniko') {
             git 'https://github.com/ab1234567/hellonode.git'
             container('Kaniko') {
                 stage('Build a project') {
-                    sh """
-                    /kaniko/executor -f `pwd`/Dockerfile -c `pwd` --insecure-skip-tls-verify --cache=true
-                    """
+                    sh /kaniko/executor -f `pwd`/Dockerfile -c `pwd` --insecure-skip-tls-verify --cache=true        
                 }
             }
         }
-
     }
 }
+    )
+}
+}
+
+public void PushtoACR(body) {
+  podTemplate(label: label,
+        containers: [containerTemplate(name: 'maven', image: 'maven', command: 'cat', ttyEnabled: true)],
+        volumes: [secretVolume(secretName: 'maven-settings', mountPath: '/root/.m2'),
+                  persistentVolumeClaim(claimName: 'maven-local-repo', mountPath: '/root/.m2nrepo')]) {
+    body()
+}
+}
+
+return this
