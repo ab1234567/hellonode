6e46517126742a86f8afbdd3aaac113fe7375abc
ab1234567 <mailtooab@gmail.com>
2018-11-15 17:23:18 +0530

    Update kaniko.groovy

--- kaniko.groovy
+++ kaniko.groovy
@@ -22,13 +22,4 @@ public void Kaniko(body) {
 }
 }
 
-public void PushtoACR(body) {
-  podTemplate(label: label,
-        containers: [containerTemplate(name: 'maven', image: 'maven', command: 'cat', ttyEnabled: true)],
-        volumes: [secretVolume(secretName: 'maven-settings', mountPath: '/root/.m2'),
-                  persistentVolumeClaim(claimName: 'maven-local-repo', mountPath: '/root/.m2nrepo')]) {
-    body()
-}
-}
-
 return this
